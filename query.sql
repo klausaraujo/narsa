8edb7843d1d7f28fa0c6629019a03896d80da1f5
klausaraujo <56495973+klausaraujo@users.noreply.github.com>
2023-01-19 22:00:14 -0500

    pene 2

--- query.sql
+++ query.sql
@@ -34,6 +34,8 @@ DROP VIEW IF EXISTS lista_ingresos_valorizaciones_saldo;
 DROP VIEW IF EXISTS lista_valorizaciones_proveedores;
 DROP VIEW IF EXISTS lista_ubigeo;
 DROP VIEW IF EXISTS lista_movimientos_caja;
+DROP VIEW IF EXISTS saldos_caja;
+
 
 CREATE TABLE ubigeo(
 	idubigeo smallint(4) NOT NULL AUTO_INCREMENT,
@@ -2500,3 +2502,10 @@ as
 select mc.idmovimiento,mc.idtipooperacion,toc.tipo_operacion,mc.idsucursal,s.sucursal,mc.idtransaccion,mc.monto,mc.interes as 'tasa',mc.monto * (mc.interes/100) as 'intereses',f.idfactor,f.factor * mc.monto as monto_factor,f.factor * ((mc.monto)+mc.monto * (mc.interes/100)) as monto_factor_final,mc.fecha_vencimiento,mc.fecha_movimiento 
 from movimientos_caja as mc inner join tipo_operacion_caja as toc on toc.idtipooperacion=mc.idtipooperacion inner join sucursal as s on s.idsucursal = mc.idsucursal inner join factor as f on f.idfactor=mc.idfactor
 where mc.activo='1';
+
+create view saldos_caja
+as
+select idsucursal,sucursal,sum(monto_factor_final) as saldo from lista_movimientos_caja group by idsucursal,sucursal;
+
+
+
